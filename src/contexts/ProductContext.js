417209f2eb1586289d36a4fa9bd1e140b31811c0
mahmoud shoura <mahmoudhameidshoura@gmail.com>
2024-03-10 12:30:14 +0200

    edit fack shop api

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { createContext, useState, useEffect } from "react";
 
-const ProductContext = () => {
-  return <div>ProductContext</div>;
+// create context
+const ProductContext = createContext();
+
+export { ProductContext };
+const ProductProvider = ({ children }) => {
+  // Product state
+  const [products, setproducts] = useState([]);
+  // fetch products
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      setproducts(data);
+    };
+    fetchProducts();
+  }, []);
+  return (
+    <ProductContext.Provider value={{ products }}>
+      {children}
+    </ProductContext.Provider>
+  );
 };
 
-export default ProductContext;
+export default ProductProvider;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useContext } from "react";
+// import product context
+import { ProductContext } from "../contexts/ProductContext";
 
 const Home = () => {
+  // get product from product context
+  const { products } = useContext(ProductContext);
+  console.log(products);
   return <div>Homepage</div>;
 };
 
