d6b74260699c3f407a3d04c85d004ad75069e883
mahmoud shoura <mahmoudhameidshoura@gmail.com>
2024-03-12 14:46:39 +0200

    edit two button to products section

--- src/components/Product.js
+++ src/components/Product.js
@@ -15,13 +15,28 @@ const Product = ({ product }) => {
           {/* image */}
           <div className="w-[200px] mx-auto flex justify-center items-center">
             <img
-              className="max-h-[160px] group-hover:scale-110"
+              className="max-h-[160px] group-hover:scale-110 transition duration-300    "
               src={image}
               alt=""
             />
           </div>
         </div>
+        {/* buttons */}
+        <div className="absolute top-6 -right-11 group-hover:right-5 p-2 flex flex-col items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
+          <button>
+            <div className="flex justify-center items-center text-white w-12 h-12 bg-red-500">
+              <BsPlus className="text-3xl" />
+            </div>
+          </button>
+          <Link
+            to={`/product/${id}`}
+            className="w-12 h-12 bg-white flex justify-center items-center text-primary drop-shadow-x1"
+          >
+            <BsEyeFill />
+          </Link>
+        </div>
       </div>
+      {/* category & title & price */}
       <div>2</div>
     </div>
   );
