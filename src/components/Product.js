7249818a8d2abe2c95d4d1b30b84d3f863a2662f
mahmoud shoura <mahmoudhameidshoura@gmail.com>
2024-03-12 14:14:54 +0200

    edit products section

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useContext } from "react";
+// import link
+import { Link } from "react-router-dom";
+// import icon
+import { BsPlus, BsEyeFill } from "react-icons/bs";
 
-const Product = () => {
-  return <div>Product</div>;
+const Product = ({ product }) => {
+  console.log(product);
+  // destructure product
+  const { id, image, category, title, price } = product;
+  return (
+    <div>
+      <div className="border border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition">
+        <div className="w-full h-full flex justify-center items-center">
+          {/* image */}
+          <div className="w-[200px] mx-auto flex justify-center items-center">
+            <img
+              className="max-h-[160px] group-hover:scale-110"
+              src={image}
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <div>2</div>
+    </div>
+  );
 };
 
 export default Product;
