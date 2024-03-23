d27544f522b05638094c3a7d57914a66a2bdf080
mahmoud shoura <mahmoudhameidshoura@gmail.com>
2024-03-23 13:47:40 +0200

    Add Remove icon to cart item & plus & minus icon

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 // mport Link
 import { Link } from "react-router-dom";
-import { IoMdClose } from "react-icons/io";
+import { IoMdAdd, IoMdClose, IoMdRemove } from "react-icons/io";
 
 const CartItem = ({ item }) => {
   // destructure item
@@ -13,9 +13,9 @@ const CartItem = ({ item }) => {
         <Link to={`/product/${id}`}>
           <img className="max-w-[80px]" src={image} alt="" />
         </Link>
-        <div>
+        <div className="w-full flex flex-col">
           {/* title & remove icon */}
-          <div>
+          <div className="flex justify-between mb-2">
             {/* title */}
             <Link
               to={`/product/${id}`}
@@ -24,9 +24,30 @@ const CartItem = ({ item }) => {
               {title}
             </Link>
             {/* remove icon */}
-            <div>
-              <IoMdClose />
+            <div className="text-xl cursor-pointer">
+              <IoMdClose className="text-gray-500 hover:text-red-500 transition" />
             </div>
+          </div>
+          <div className="flex gap-x-2 h-[36px] text-sm">
+            {/* qty */}
+            <div className="flex flex-1 max-w-[100px] items-center h-full border text-primary font-medium">
+              {/* minus icon */}
+              <div className="flex-1 flex justify-center items-center cursor-pointer">
+                <IoMdRemove />
+              </div>
+              {/* amount */}
+              <div className="h-full flex justify-center items-center px-2">
+                {amount}
+              </div>
+              {/* plus icon */}
+              <div className="flex-1 h-full flex justify-center items-center cursor-pointer">
+                <IoMdAdd />
+              </div>
+            </div>
+            {/* item price */}
+            <div>item price</div>
+            {/* final price */}
+            <div>final price</div>
           </div>
         </div>
       </div>
