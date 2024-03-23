10a1f99a1966d15dd0fc9db2cd43608f54cfa6a1
mahmoud shoura <mahmoudhameidshoura@gmail.com>
2024-03-23 21:42:58 +0200

    Create Product Details Page

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -7,6 +7,18 @@ import { CartContext } from "../contexts/CartContext";
 import { ProductContext } from "../contexts/ProductContext";
 
 const ProductDetails = () => {
+  // get the product id from the url
+  const { id } = useParams();
+  const { products } = useContext(ProductContext);
+  const { addToCart } = useContext(CartContext);
+
+  // get the single product based on the id
+  const product = products.find((item) => {
+    return item.id === parseInt(id);
+  });
+
+  console.log(product);
+
   return <div>Product Details Page</div>;
 };
 
